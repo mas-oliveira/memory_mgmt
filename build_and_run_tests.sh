72fb6ae6b3250447b26d27292e3192722ada325f
Marco Oliveira <62846180+mas-oliveira@users.noreply.github.com>
2025-11-17 22:06:16 +0000

    Add CH4 builddir settings

--- build_and_run_tests.sh
+++ build_and_run_tests.sh
@@ -11,7 +11,14 @@ meson compile -C builddir || exit 1
 # Percorre todos os ficheiros executáveis dentro de builddir/ch2_structs/
 #find builddir/ch2_structs -type f -perm +111 -exec echo "▶️ A correr {}" \; -exec {} \;
 
-echo "🚀 A correr todos os testes em subdiretórios de ch3_pointers..."
+
+#echo "🚀 A correr todos os testes em subdiretórios de ch3_pointers..."
 
 # Percorre todos os ficheiros executáveis dentro de builddir/ch3_pointers/
-find builddir/ch3_pointers -type f -perm +111 -exec echo "▶️ A correr {}" \; -exec {} \;
+#find builddir/ch3_pointers -type f -perm +111 -exec echo "▶️ A correr {}" \; -exec {} \;
+
+
+echo "🚀 A correr todos os testes em subdiretórios de ch4_enums..."
+
+# Percorre todos os ficheiros executáveis dentro de builddir/ch3_pointers/
+find builddir/ch4_enums -type f -perm +111 -exec echo "▶️ A correr {}" \; -exec {} \;
